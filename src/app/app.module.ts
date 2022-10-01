3a275f88cb64b58249f4a24e22030b684e1dfc52
Hasan-KHALID <khalidhasan842@gmail.com>
2022-10-01 22:38:00 +0600

    template added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
+import { ItemComponent } from './body/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeaderComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
